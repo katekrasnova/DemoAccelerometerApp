7f09fd61f98d131730e37efd10415cfe7ffff38a
katekrasnova <katekrasnova1990@gmail.com>
2016-04-29 10:52:03 +0300

    Initial Commit

---
+++ AccelerometerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AccelerometerDemo
+//
+//  Created by Ekaterina Krasnova on 29.04.16.
+//  Copyright © 2016 Ekaterina Krasnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AccelerometerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccelerometerDemo
+//
+//  Created by Ekaterina Krasnova on 29.04.16.
+//  Copyright © 2016 Ekaterina Krasnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
